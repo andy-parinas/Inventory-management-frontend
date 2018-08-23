7469e60f8aba6f73b39c92fa5418569bb6cdcfc4
Andy Parinas <andy.parinas@gmail.com>
2018-08-23 13:52:04 +1000

    Inital Commit. Setting the Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationHeaderComponent } from './common/navigation-header/navigation-header.component';
+import { PageHeaderComponent } from './common/page-header/page-header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoriesComponent } from './inventories/inventories.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationHeaderComponent,
+    PageHeaderComponent,
+    DashboardComponent,
+    InventoriesComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule
